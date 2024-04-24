e2c54150ab84280d22c3d45c9b09f0d36c2000c7
NathanLuizz <nathan.aquino@sga.pucminas.br>
2024-04-24 19:20:56 -0300

    Adicionar arquivos de projeto.

---
+++ Aspnet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aspnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
