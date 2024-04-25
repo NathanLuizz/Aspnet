9d8ecc0184bde2d658d79177ffde94c374c76397
NathanLuizz <nathan.aquino@sga.pucminas.br>
2024-04-25 16:27:09 -0300

    Config do banco de dados e da tabela veiculo

---
+++ Aspnet/Models/AppDbcontext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Aspnet.Models
+{
+    public class AppDbcontext : DbContext
+    {
+        public AppDbcontext(DbContextOptions<AppDbcontext> options) : base(options) { }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+
+    }
+}
